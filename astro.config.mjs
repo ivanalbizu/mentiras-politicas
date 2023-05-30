8022c2c8390d461413840f8cfd48dc010310507a
ivanalbizu <mariano-ivan.gonzalez-albizu@capgemini.com>
2023-05-30 07:52:32 +0200

    feat: config netlify cms

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,5 @@
 import { defineConfig } from 'astro/config';
+import NetlifyCMS from 'astro-netlify-cms';
 import mdx from '@astrojs/mdx';
 
 import sitemap from '@astrojs/sitemap';
@@ -6,5 +7,29 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+	integrations: [mdx(), sitemap(),
+		NetlifyCMS({
+      config: {
+				// Use Netlify’s “Git Gateway” authentication and target our default branch
+				backend: {
+					name: 'git-gateway',
+					branch: 'main',
+				},
+				collections: [
+					// Define a blog post collection
+					{
+						name: 'posts',
+						label: 'Blog Posts',
+						folder: 'src/pages/blog',
+						create: true,
+						delete: true,
+						fields: [
+							{ name: 'title', widget: 'string', label: 'Post Title' },
+							{ name: 'body', widget: 'markdown', label: 'Post Body' },
+						],
+					},
+				],
+			};
+    }),
+	],
 });
